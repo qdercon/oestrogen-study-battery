e0314eb7bb365befd916a72dd84f3d5d7e3678ac
Quentin Dercon <quentindercon@gmail.com>
2026-09-24 13:00:43 +0100

    v0.1.0

--- tasks/max-press-test/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Core max-press-test task index - centralizes all function exports
- * Provides a single entry point for importing max-press-test task functions across the experiment.
- * Even though this is a very simple task, this index file maintains consistency with other tasks that do have multiple modules.
- */
-
-// Import and re-export all utility modules
-export * from './task.js';
